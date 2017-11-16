c621b9dfe486558527deb9749c2a239fab13b8e9
adira9 <30999036+adira9@users.noreply.github.com>
2017-11-16 08:02:02 +0530

    Commenting done

--- ChatClientThread.java
+++ ChatClientThread.java
@@ -1,16 +1,21 @@
 import java.net.*;
 import java.io.*;
+
+
 public class ChatClientThread extends Thread{
 	private Socket           socket   = null;
 	   private ChatClient       client   = null;
 	   private DataInputStream  streamIn = null;
-
+	  
+	   //Constructor creates and runs the client and starts connection to server		
 	   public ChatClientThread(ChatClient _client, Socket _socket)
 	   {  client   = _client;
 	      socket   = _socket;
 	      open();  
 	      start();
 	   }
+	
+	   //Opens input stream from client side socket.
 	   public void open()
 	   {  try
 	      {  streamIn  = new DataInputStream(socket.getInputStream());
@@ -20,6 +25,8 @@ public class ChatClientThread extends Thread{
 	         client.stop();
 	      }
 	   }
+	
+	   //processes to close server connection
 	   public void close()
 	   {  try
 	      {  if (streamIn != null) streamIn.close();
@@ -28,6 +35,8 @@ public class ChatClientThread extends Thread{
 	      {  System.out.println("Error closing input stream: " + ioe);
 	      }
 	   }
+	
+	   //run:- calls .handle() method on data written to console.
 	   public void run()
 	   {  while (true)
 	      {  try
